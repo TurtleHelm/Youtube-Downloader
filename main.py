930bd8a11f923d03062db21e76c25070b0fcb94f
Helm <aaronsfwilson@gmail.com>
2021-10-13 16:33:34 +0100

    Main Files

---
+++ main.py
@@ -0,0 +1,27 @@
+from pytube import YouTube
+import shutil
+import os
+
+link = input('Enter Link: ')
+yt = YouTube(link)
+
+print(f'Title: {yt.title}\nNumber of Views: {yt.views}\nLength of Video: {yt.length}\nRating: {yt.rating}')
+
+isAudioOnly = input("Audio Only?: ").lower()
+
+path = 'C:Users/aaron/Desktop/YouTube Video Downloader'
+
+if isAudioOnly == 'yes':
+    ys = yt.streams.get_audio_only()
+    print('Downloading...')
+    ys.download()
+    os.rename(f'{yt.title}.mp4', f'{yt.title}.mp3')
+    shutil.move(f'{yt.title}.mp3', f'Music/{yt.title}.mp3')
+    print('Download Complete')
+
+else:
+    ys = yt.streams.get_highest_resolution()
+    print('Downloading, This may take a while due to large file sizes...')
+    ys.download()
+    shutil.move(f'{yt.title}.mp4', f'Videos/{yt.title}.mp4')
+    print('Download Complete')
