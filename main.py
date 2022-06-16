3fe0f666146b893e379c476d0787931a4910728c
Helm <89736173+TurtleHelm@users.noreply.github.com>
2022-06-16 11:18:22 +0100

    minor changes

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
+from tkinter.filedialog import askDirectory
 from pytube import YouTube
 import shutil
 import os
 
-link = input('Enter Link: ')
-yt = YouTube(link)
 
-print(f'Title: {yt.title}\nNumber of Views: {yt.views}\nLength of Video: {yt.length/60:.2f}\nRating: {yt.rating:.2f}')
+def DownloadYT():
+    link = input('Enter Link: ')
+    yt = YouTube(link)
 
-isAudioOnly = input("Audio Only?: ").lower()
+    print(f'Title: {yt.title}\nNumber of Views: {yt.views}\nLength of Video: {yt.length/60:.2f}\nRating: {yt.rating:.2f}')
 
-path = 'Path To Folder'
+    isAudioOnly = input("Audio Only?: ").lower()
 
-if isAudioOnly == 'yes':
-    ys = yt.streams.get_audio_only()
-    print('Downloading...')
-    ys.download()
-    os.rename(f'{yt.title}.mp4', f'{yt.title}.mp3')
-    shutil.move(f'{yt.title}.mp3', f'Music/{yt.title}.mp3')
-    print('Download Complete')
+    path = askDirectory()
 
-else:
-    ys = yt.streams.get_highest_resolution()
-    print('Downloading, This may take a while due to large file sizes...')
-    ys.download()
-    shutil.move(f'{yt.title}.mp4', f'Videos/{yt.title}.mp4')
-    print('Download Complete')
+    if isAudioOnly == 'yes':
+        ys = yt.streams.get_audio_only()
+        print('Downloading...')
+        
+        ys.download()
+        os.rename(f'{yt.title}.mp4', f'{yt.title}.mp3')
+
+        shutil.move(f'{yt.title}.mp3', f'Music/{yt.title}.mp3')
+        print('Download Complete')
+
+    else:
+        ys = yt.streams.get_highest_resolution()
+        print('Downloading, This may take a while due to large file sizes...')
+
+        ys.download()
+
+        shutil.move(f'{yt.title}.mp4', f'Videos/{yt.title}.mp4')
+        print('Download Complete')
